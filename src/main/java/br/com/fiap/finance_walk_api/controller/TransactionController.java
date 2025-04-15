7d501783791d2e142bedff275a78d001f4727322
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-15 20:39:17 -0300

    WIP: Fornecendo paginação ao Front

--- src/main/java/br/com/fiap/finance_walk_api/controller/TransactionController.java
+++ src/main/java/br/com/fiap/finance_walk_api/controller/TransactionController.java
@@ -7,7 +7,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.ExampleMatcher.StringMatcher;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,9 +31,10 @@ public class TransactionController {
     private TransactionRepository repository;
 
     @GetMapping
-    public List<Transaction> index(TransactionFilter filter) {
-       
-        return repository.findAll(TransactionSpecification.withFilters(filter));
+    public Page<Transaction> index(TransactionFilter filter,@PageableDefault(size = 10) Pageable pageable) {
+       //Pageable pageable - Fornecendo paginação para o Front
+       // a Anatoção @PageableDefault(size = 10) - Define a qtd de registros por pág 
+        return repository.findAll(TransactionSpecification.withFilters(filter), pageable);
     }
 
 }
